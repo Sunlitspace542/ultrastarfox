25ef9ea1322b64278a50c87e350933521c033b37
sunlitspace542 <rabbidsluis123@gmail.com>
2022-08-14 11:38:34 -0400

    tex_32

--- optionalstuff/textures/final (tex)/make.bat
+++ optionalstuff/textures/final (tex)/make.bat
@@ -1,5 +1,5 @@
 del *.bin
 merge.exe tex_0.CGX tex_1.CGX tex_01.bin /cgx
-merge.exe tex_3.CGX tex_2.CGX tex_32.bin /cgx
-merge.exe tex_3_a.CGX tex_2.CGX tex_32_a.bin /cgx
+merge.exe tex_2.CGX tex_3.CGX tex_23.bin /cgx
+merge.exe tex_2.CGX tex_3_a.CGX tex_23_a.bin /cgx
 exit
