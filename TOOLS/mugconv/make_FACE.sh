d7f825060aad135b3d9d40000a33d0d377751da7
repzilon <6383002+repzilon@users.noreply.github.com>
2024-08-13 08:43:35 -0400

    Added shebangs to shell scripts

--- TOOLS/mugconv/make_FACE.sh
+++ TOOLS/mugconv/make_FACE.sh
@@ -1,3 +1,5 @@
+#! bin/sh
+
 superfamiconv/superfamiconv tiles -i FACE.png -p superfamiconv/mugs-transparent.pal --out-data FACE.CGX --bpp 4 --no-discard
 ./foxchr FACE.CGX
 rm -rf FACE.CGX
--- commit.sh
+++ commit.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 echo "Enter commit description:"
 read desc
 echo "Enter branch:"
