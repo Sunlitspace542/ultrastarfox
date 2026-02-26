02f78ebd5dc3c469c5d9647482a2d70bebded949
SunlitSpace542 <35207254+Sunlitspace542@users.noreply.github.com>
2026-02-26 17:00:16 -0500

    patch sound driver for MSU-1 instead of using a prepatched copy

---
+++ SF/SND/bgmmute.asm
@@ -0,0 +1,35 @@
+; Asar 1.91
+norom
+
+org $0000
+
+; ===========================================
+; patch sound driver to mute BGM for MSU-1
+; assemble with
+; asar --no-title-check bgmmute.asm bgmmute.bin
+!BASE_ADDR = $08A3	; location of sound $60 pattern table in ARAM
+dw patch_data_end-patch_data_start		; calculate size in bytes
+dw !BASE_ADDR								; spc destination
+
+
+patch_data_start:
+
+; context of the driver code we're patching:
+; tx17:
+; 	push	a
+; 	mov.b	a,!keyd
+; 	and.b	a,!fkin
+; 	pop	a
+; 	bne	tx18 ; <- this instruction will become a BRA instead
+; 	call	dss
+
+	db	$2F
+
+patch_data_end:
+; ===========================================
+
+; ============================
+; end of data, start execution
+; ============================
+dw $0000
+dw $0400									; start execution here
